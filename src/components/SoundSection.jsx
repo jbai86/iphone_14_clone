705a97a3937e0128362a8237672bcc5e2888aa6e
Jon Bai <jowned1@gmail.com>
2023-08-10 17:31:39 -0300

    initial commit of apple clone website effect using react + webgi

---
+++ src/components/SoundSection.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+function SoundSection() {
+
+    const handleLearnMore = () => {
+
+        const element = document.querySelector('.display-section');
+
+        window.scrollTo({
+            top: element?.getBoundingClientRect().bottom,
+            left: 0,
+            behavior: 'smooth',
+        })
+    }
+
+    return ( 
+       <div className="sound-section wrapper">
+           <div className="body">
+                <div className="sound-section-content content">
+                    <h2 className="title">New Sound System</h2>
+                    <p className="text">Teel the base.</p>
+                    <span className="description">
+                        From 41.62/mo. for mo. $999 before trade-in
+                    </span>
+                    <ul className="links">
+                        <li>
+                            <button className="button">Buy</button>
+                        </li>
+                        <li>
+                            <a onClick={handleLearnMore} className="link">Learn More</a>
+                        </li>
+                    </ul>
+                </div>   
+           </div>
+       </div> 
+     );
+}
+
+export default SoundSection;
